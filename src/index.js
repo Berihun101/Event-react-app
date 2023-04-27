0ea458746232aff36ff1d1678d68ccffb7cc5de3
my account <taberihun07@gmail.com>
2023-04-27 01:32:41 -0700

    intial commit of my app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './componants/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootEl=document.getElementById("root")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function reinder(){
+    ReactDOM.render(<App />, rootEl )
+}
+
+if(module.hot){
+    module.hot.accept('./componants/App', ()=>{
+        setTimeout(reinder)
+    })
+}
+reinder();
+
+
+
+
